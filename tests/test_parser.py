2f7289381acd0b988211e807a950db2eaa37c0d8
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2023-10-03 10:08:47 -0400

    add find_repo_url

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -5,7 +5,7 @@ from git_web_url.tests.data import CaseEnum
 from git_web_url.parser import parse
 
 
-def _test_get_repo_url_all():
+def _test_parse_all():
     for name, case in CaseEnum.iter_items():
         # print(f"{name}: {case.origin_url}")
         result = parse(case.origin_url)
@@ -16,20 +16,20 @@ def _test_get_repo_url_all():
         assert result.repo == case.repo
 
 
-def _test_get_repo_url_edge_case():
+def _test_parse_edge_case():
     case = CaseEnum.aws_codecommit_grc.value
     result = parse(case.origin_url, debug=True)
-    print(f"protocol: {[result.protocol, case.protocol]}")
-    print(f"platform: {[result.platform, case.platform]}")
-    print(f"domain: {[result.domain, case.domain]}")
-    print(f"owner: {[result.owner, case.owner]}")
-    print(f"repo: {[result.repo, case.repo]}")
+    print(f"protocol: {result.protocol!r} vs {case.protocol!r}")
+    print(f"platform: {result.platform!r} vs {case.platform!r}")
+    print(f"domain: {result.domain!r} vs {case.domain!r}")
+    print(f"owner: {result.owner!r} vs {case.owner!r}")
+    print(f"repo: {result.repo!r} vs {case.repo!r}")
 
 
 def test_get_repo_url():
     print("")
-    _test_get_repo_url_all()
-    # _test_get_repo_url_edge_case()
+    _test_parse_all()
+    # _test_parse_edge_case()
 
 
 if __name__ == "__main__":
